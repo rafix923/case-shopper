ab0d269d5fe0d0118da31adb33bfe08fdffe6a13
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-06-12 02:42:46 -0300

    feat: Cria endpoint para criação do pedido.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import app from "./app";
 import { addClient } from "./endpoints/addClient";
+import { addNewOrder } from "./endpoints/addNewOrder";
 import { listAllClients } from "./endpoints/listAllClients";
 import { listAllProducts } from "./endpoints/listAllProducts";
 
 app.post('/client/add', addClient);
 app.get('/client/list', listAllClients);
 app.get('/products/list', listAllProducts);
+app.post('/order/add/new', addNewOrder);
