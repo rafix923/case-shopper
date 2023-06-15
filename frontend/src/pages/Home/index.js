fb5e651684c93cd2a961d293698349ddb01eeac8
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-06-15 16:25:13 -0300

    chore(build): Cria a estrutura do componente em que será exposto o pedido do produto.

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import Form from "../../components/Form";
+import Product from "../../components/Product";
 
 function Home() {
   let title = "Boas vindas ao nosso Hortifruit";
@@ -8,6 +9,8 @@ function Home() {
     <div>
       <Header />
       <h1>{title}</h1>
+      <Product />
+      <hr/>
       <Form />
     </div>
   );
