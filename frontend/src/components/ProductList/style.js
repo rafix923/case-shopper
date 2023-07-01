99d0610f6e22a99be8f7da919d0eeff608b8bc77
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-01 02:13:54 -0300

    styled: Acrescenta estilo e aumenta o tamanho da font para o componente ProductList.

--- frontend/src/components/ProductList/style.js
+++ frontend/src/components/ProductList/style.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
-import { secondaryColor } from "../../constants/colors/colors";
-
 
 export const OrderList = styled.div`
 display: flex;
 justify-content: center;
 width: 100vw;
 color: #fff;
+`;
+
+export const Sum = styled.p`
+font-size: 1.2rem;
+font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 `;
