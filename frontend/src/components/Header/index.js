545aff73e336c2aab1fe0e7517e471bff1cfbdf7
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-02 04:08:00 -0300

    feat: Acesso da página de estoque com botão no header, renderizadopela página home.

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { HeaderStyled } from "./style";
+import { useNavigate } from "react-router-dom";
+import { goToInventoryPage } from "../../routes/Coordinator";
 
 function Header() {
+  const navigate = useNavigate();
   let title = "Case Shopper Hortifruit";
   return (
     <HeaderStyled>
       <h1 id="header-title">{title}</h1>
+      <button type="button" onClick={() => goToInventoryPage(navigate)}>
+        Ver estoque
+      </button>
     </HeaderStyled>
   );
 }
