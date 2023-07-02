caef643ade10dec40a151891d7a81157b8e6f2ac
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-02 02:01:53 -0300

    feat: Adiciona função goToReturnPage napágina de inventário para retornar à página anterior.

--- frontend/src/pages/InventoryPage/inventory.js
+++ frontend/src/pages/InventoryPage/inventory.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { goToReturnPage } from "../../routes/Coordinator";
 
 function InventoryPage() {
-  return <div>
-    <h1>Inventory</h1>
-  </div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Inventory</h1>
+      <button type="button" onClick={() => goToReturnPage(navigate)}>
+        Retornar
+      </button>
+    </div>
+  );
 }
 
 export default InventoryPage;
