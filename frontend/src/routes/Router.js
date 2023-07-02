2f2df3972530e71e7415a4b25cd769a4a0887cd7
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-02 01:35:13 -0300

    feat: Adiciona a página inventory para mostrar o estoque e cria rota.
    Foi adicionado lógica para que o botão de ver estoque só apareça após escolher um produto da lista.

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Farewell from "../pages/Farewell";
+import InventoryPage from "../pages/InventoryPage/inventory";
 
 function Router() {
   return (
@@ -8,6 +9,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="end-page" element={<Farewell />} />
+        <Route path="inventory" element={<InventoryPage />} />
       </Routes>
     </BrowserRouter>
   );
